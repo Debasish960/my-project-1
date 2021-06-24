42aaca422b9c9f3806dc5f4619f5155a21c070f5
Debasish960 <79779204+Debasish960@users.noreply.github.com>
2021-06-24 23:43:47 +0530

    Delete Notes/Terraform directory

--- Notes/Terraform/main.tf
+++
@@ -1,8 +0,0 @@
-
-provider "aws" {
-  region                  = "${var.region}"
-  shared_credentials_file = "~/.aws/credentials"
-  # profile                 = "development"
-  # private_key_path = "~/vilas.pem"
-}
-
